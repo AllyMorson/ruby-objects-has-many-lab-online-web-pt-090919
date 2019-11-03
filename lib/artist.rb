ed80a340c2488fd07d5a1aa7e0fb52fda97c14c0
Ally Morson <ally.morson@gmail.com>
2019-11-03 16:45:12 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,27 @@
 class Artist
   attr_accessor :name
 
-  @@all = []
+  @@all = [] #keeps track of the many songs and is placed before initialize
 
-  def initialize(name)
+def initialize(name) #initilized with a name arugmement 
     @name = name
-    @@all << self
+    @@all << self #push all instances into array 
   end
-
-  def self.all
-    @@all
-  end
-
-  def add_song(song)
-    song.artist = self
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
-
-  def songs
+  
+def add_song(song) #add_song takes in an argument of a song 
+  song.artist = self #associates that song with the artist by telling the song that it belongs to that artist
+  end 
+  
+def add_song_by_name(name)  
+    song = Song.new(name) #creates a new song with it
+    song.artist = self #associates the song and artist by telling the song that it belongs to that artist
+  end 
+  
+def songs
     Song.all.select {|song| song.artist == self}
   end
 
-  def self.song_count
+def self.song_count
     Song.all.count
   end
 end
--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,23 @@
-end
+class Author 
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def add_post(post)
+    post.author = self #akes in an argument of a post and associates that post with the author by telling the post that it belongs to that author
+  end 
+  
+  def add_post_by_title(title) #takes in an argumentof a post title, 
+    post = Post.new(title) #creates a new post with it 
+    post.author = self #associates the post and author
+  end 
+  
+  def self.post
+Author.all.select {|post| post.author == self} #returns the total number of posts associated to all existing authors
+end 
+
+  

b3d56f7420cdc397d6ce47a2b666cad7029cf94d
Ally Morson <ally.morson@gmail.com>
2019-11-03 17:18:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
 class Song 
-  
+  attr_accessor :name , :artist 
   
   @@all = []
+  
   def initialize(name)
     @name = name 
     @@all << self #new pushes new instances into a class variable called @@all upon initialization
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+end 
